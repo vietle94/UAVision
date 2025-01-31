3b38b694a05821fca36fe12ce9cd1959483f300d
Le Viet (FMI) <viet.le@fmi.fi>
2025-01-31 10:52:46 +0200

    add calculate lag

--- src/UAVision/mavic/preprocess.py
+++ src/UAVision/mavic/preprocess.py
@@ -1,5 +1,6 @@
 import numpy as np
 import importlib.resources
+import pandas as pd
 
 n2_binedges = (
     importlib.resources.files("UAVision.mavic")
@@ -32,3 +33,16 @@ def calculate_concentration(df, bin_label, flow_label=None, period_label=None):
         concentration = df[bin_label].div(total_volume, axis=0).div(period, axis=0)
         dndlogdp = concentration.div(dlog_bin, axis=1)
         return concentration, dndlogdp
+
+def calculate_lag(df, var1, var2, lag):
+    """Calculate the lag between two variables, same dataframe.
+    Please resample the dataframe so spacing is consistent"""
+    lag_range = np.arange(-lag, lag+1)
+    df_corr = pd.DataFrame({
+        'covariance': np.array([df[var1].corr(df[var2].shift(x)) for x in lag_range]),
+        'lag': lag_range})
+    df_corr['covariance'] = df_corr['covariance'].abs()
+    imax = df_corr['covariance'].idxmax()
+    lag_max = df_corr['lag'][imax]
+    print(f"Max correlation when shift forward {var2} by {lag_max} seconds")
+    return lag_max
